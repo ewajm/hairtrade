7a7da45f1e8137f3f450eeeb6d68fd5e22a36382
Ewa Manek <manekej@gmail.com>
2021-11-11 12:49:34 -0800

    add users table

--- backend/app/db/metadata.py
+++ backend/app/db/metadata.py
@@ -1,6 +1,11 @@
 from sqlalchemy import *
+from datetime import datetime
 
 metadata_obj = MetaData()
+
+def mixin_factory():
+    return (Column("created_at", TIMESTAMP, default=datetime.utcnow, nullable=False),\
+        Column("updated_at", TIMESTAMP, default=datetime.utcnow, onupdate=datetime.utcnow, nullable=False))
 
 products = Table("products", 
     metadata_obj,
@@ -11,4 +16,20 @@ products = Table("products",
     Column("type", Text, nullable=False, server_default="idk, a bottle"),
     Column("what_do", Text, nullable=False, server_default="trade"),
     Column("price", Numeric(10,2), nullable=True),
+    *mixin_factory()
 )
+
+users = Table("users", 
+    metadata_obj,
+    Column("id", Integer, primary_key=True),
+    Column("username", Text, unique=True, nullable=False, index=True),        
+    Column("email", Text, unique=True, nullable=False, index=True),
+    Column("email_verified", Boolean, nullable=False, server_default="False"),
+    Column("salt", Text, nullable=False),
+    Column("password", Text, nullable=False),
+    Column("is_active", Boolean(), nullable=False, server_default="True"),
+    Column("is_superuser", Boolean(), nullable=False, server_default="False"),  
+    *mixin_factory()
+)
+
+
